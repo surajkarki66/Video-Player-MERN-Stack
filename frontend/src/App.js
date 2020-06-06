459c249b847b7b5393fdd0caece016c9d3eb6259
Suraj Karki <suraj.karki500@gmail.com>
2020-06-06 23:02:43 +0545

    Add SignOut.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 
 import SignUp from './components/Form/SignUp/SignUp';
 import SignIn from './components/Form/SignIn/SignIn';
+import SignOut from './components/SignOut/SignOut';
 
 function App() {
   return (
     <React.Fragment>
       <Route exact path="/signIn" component={SignIn} />
       <Route exact path="/signUp" component={SignUp} />
+      <Route exact path="/signOut" component={SignOut} />
     </React.Fragment>
   );
 }
