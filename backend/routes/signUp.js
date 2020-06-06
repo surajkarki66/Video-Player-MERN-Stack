3bc60cddb58f3968a53e6bab314f4b9b322a8162
Suraj Karki <suraj.karki500@gmail.com>
2020-06-06 13:54:32 +0545

    Finished signUp.js

--- backend/routes/signUp.js
+++ backend/routes/signUp.js
@@ -1,9 +1,53 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
-router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Message recieved from post on /api/signUp'
+const User = require("../models/User");
+
+router.post("/", (req, res) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "email already exists!",
+        });
+      } else {
+        // Generate new user
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              email: req.body.email,
+              password: hash,
+            });
+            user
+              .save()
+              .then((result) => {
+                res.status(201).json({
+                  message: "User Created",
+                });
+              })
+              .catch((err) => {
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(422).json({
+        error: err,
+      });
     });
 });
 
